81960dad9da1423ee02e32cda6546705d6368ac7
Caspartai <taikalungcc@gmail.com>
2021-07-26 19:56:27 +0800

    git add .

--- migrations/20210714082038_post.js
+++ migrations/20210714082038_post.js
@@ -5,15 +5,8 @@ exports.up = function(knex) {
         table.string('owner_name');
         table.json('content');
         table.timestamp('created_at').defaultTo(knex.fn.now());
-<<<<<<< HEAD
-
-        table.string('type');
-        // table.foreign('owner_imgPath').references('user.imgPath');
-        table.string('owen_imgPath');
-=======
         table.string('visible_group');
         table.foreign('owner_name').references('user.username');
->>>>>>> c1dc22783df71dbe303efd0d0d234cc502f7186c
   })
 };
 
